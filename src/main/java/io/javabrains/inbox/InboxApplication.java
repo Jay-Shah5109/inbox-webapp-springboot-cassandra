a379b7edcfbb92f7d06c9149ad7493fdc72d780c
jay <jayshah5109@gmail.com>
2022-09-01 17:47:09 +0530

    created folders per user

--- src/main/java/io/javabrains/inbox/InboxApplication.java
+++ src/main/java/io/javabrains/inbox/InboxApplication.java
@@ -1,18 +1,29 @@
 package io.javabrains.inbox;
 
+import io.javabrains.inbox.folders.Folder;
+import io.javabrains.inbox.folders.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class InboxApplication {
+
+	@Autowired
+	FolderRepository folderRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InboxApplication.class, args);
@@ -21,8 +32,17 @@ public class InboxApplication {
 	@Bean
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties
 																		dataStaxAstraProperties) {
+		System.out.println("datastax astra prop:"+dataStaxAstraProperties.getSecureConnectBundle().toPath());
 		Path bundle = dataStaxAstraProperties.getSecureConnectBundle().toPath();
+		System.out.println("Bundle:"+bundle);
 		return cqlSessionBuilder -> cqlSessionBuilder.withCloudSecureConnectBundle(bundle);
+	}
+
+	@PostConstruct
+	public void init() {
+		Folder folder = new Folder("Jay-Shah5109", "Inbox", "blue");
+		folderRepository.save(folder);
+		folderRepository.save(new Folder("Jay-Shah5109", "Sent", "red"));
 	}
 
 }
